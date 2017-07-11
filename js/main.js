9583add26c5ca9dd33db22006c4df8d2d53d1d0c
roman <s0553506@htw-berlin.de>
2017-07-11 17:58:22 +0200

    implement cloning of the object and setting up a refraction map

--- js/main.js
+++ js/main.js
@@ -1,4 +1,4 @@
-var camera, scene, renderer, mainObject, standardMaterial, startTime, spotLight, spotLight2, cameraTarget;
+var camera, scene, renderer,dummy, mainObject, standardMaterial, startTime, spotLight, spotLight2, cameraTarget, wasSplit, wasReflected;
 var lastFrame = null;
 init();
 
@@ -8,9 +8,6 @@ function init() {
     renderer = new THREE.WebGLRenderer();
     renderer.setSize( window.innerWidth, window.innerHeight );
 
-    renderer.shadowMap.enabled = true;
-    renderer.shadowMap.type = THREE.PCFSoftShadowMap;
-    renderer.shadowMapSoft = true;
 
     // renderer.gammaInput = true;
     // renderer.gammaOutput = true;
@@ -19,51 +16,54 @@ function init() {
     // orbitControls = new THREE.OrbitControls(camera, renderer.domElement);
 
     // planeMaterial = new THREE.MeshStandardMaterial( {color:0xFFFFFF, roughness:1, metalness:0.6} );
-    standardMaterial = new THREE.MeshPhongMaterial( {color:0x80FFB7, shading:THREE.FlatShading, dithering:false} );
+    // standardMaterial = new THREE.MeshPhongMaterial( {color:0x80FFB7, shading:THREE.FlatShading, dithering:false} );
+
+    var spongebobMap = new THREE.CubeTextureLoader()
+        .setPath( './tex/spongebob/' )
+        .load( [ 'px.png', 'nx.png', 'py.png', 'ny.png', 'pz.png', 'nz.png' ] );
+    spongebobMap.mapping = THREE.CubeRefractionMapping;
+
+    standardMaterial = new THREE.MeshPhongMaterial( {color:0xFFFFFF,shininess:0, shading:THREE.FlatShading, reflectivity:0.0, dithering:false, envMap:spongebobMap} );
     planeMaterial = new THREE.MeshPhongMaterial( {color:0xF49CD2, shading:THREE.FlatShading, dithering:false} );
 
     scene = new THREE.Scene();
+    scene.background = new THREE.Color( 0xF3D0E6 );
     startTime = performance.now();
 
-    // var hemisphereLight = new THREE.HemisphereLight( 0xFFFFFF, 0xFFFFFF, 0.6 );
-    // scene.add( hemisphereLight );
 
-    spotLight = new THREE.SpotLight(0xFFFFFF,0.6, 300, toRadians(15), 0.7,0.1);
-    spotLight.position.x = -24.8;
-    spotLight.position.z = -24.8;
-    spotLight.position.y = 32;
+    spotLight = new THREE.SpotLight(0x80FFB7,0.9, 300, toRadians(20), 1,0);
+    spotLight.position.x = -18.8;
+    spotLight.position.z = -28.8;
+    spotLight.position.y = 20;
     spotLight.target.position.set(0,0,0);
-
-    spotLight.castShadow = true;
-
-    spotLight.shadow.mapSize.width = 512;
-    spotLight.shadow.mapSize.height = 512;
-    spotLight.shadow.camera.near = 25;
-    spotLight.shadow.camera.far = 150;
 
     scene.add(spotLight);
     scene.add(spotLight.target);
 
-    var helper = new THREE.CameraHelper( spotLight.shadow.camera );
-    // scene.add( helper );
-
-    var lightHelper = new THREE.SpotLightHelper( spotLight );
-    // scene.add( lightHelper );
-
-    spotLight2 = new THREE.SpotLight(0xFFFFFF,0.4,20, toRadians(180), 0.3, 0);
-    spotLight2.position.z = -24.4;
-    spotLight2.position.y = -12;
-    // scene.add(spotLight2);
-    spotLight2.target.position.set(0,0,0);
+    spotLight2 = new THREE.SpotLight(0xf37cbe,1, 300, toRadians(25), 1,0);
+    spotLight2.position.x = 16;
+    spotLight2.position.z = -2;
+    spotLight2.position.y = -4;
+    spotLight2.target.position.set(0,4,0);
+    scene.add(spotLight2);
     scene.add(spotLight2.target);
 
-    var plane = new THREE.Mesh(new THREE.PlaneGeometry(8000,8000), planeMaterial);
-    plane.position.y = -8;
-    plane.rotation.x =toRadians(-90);
 
-    plane.receiveShadow = true;
+    spotLight3 = new THREE.SpotLight(0xB57FFF,0.8, 300, toRadians(25), 1,0);
+    spotLight3.position.x = 16;
+    spotLight3.position.z = -22;
+    spotLight3.position.y = -8;
+    spotLight3.target.position.set(0,-9,0);
 
-    scene.add(plane);
+    scene.add(spotLight3);
+    scene.add(spotLight3.target);
+
+
+    // var helper = new THREE.CameraHelper( spotLight.shadow.camera );
+    // scene.add( helper );
+
+    // var lightHelper = new THREE.SpotLightHelper( spotLight );
+    // scene.add( lightHelper );
 
 
     document.getElementById("canvas_wrapper").appendChild( renderer.domElement );
@@ -85,35 +85,82 @@ function placeMainObject(){
     jsonLoader.load(
 
         // resource URL
-            './models/ananas.json',
+        './models/ananas.json',
 
-            // Function when resource is loaded
-            function ( geometry, materials ) {
-                var material = materials[ 0 ];
-                var bufferGeometry = new THREE.BufferGeometry();
-                bufferGeometry.fromGeometry(geometry);
-                mainObject = new THREE.Mesh( bufferGeometry, standardMaterial);
+        // Function when resource is loaded
+        function ( geometry, materials ) {
+            var material = materials[ 0 ];
+            var bufferGeometry = new THREE.BufferGeometry();
+            bufferGeometry.fromGeometry(geometry);
+            mainObject = new THREE.Mesh( bufferGeometry, standardMaterial);
+            dummy = new THREE.Mesh(new THREE.CubeGeometry(.001, .001, .001), standardMaterial);
+            // dummy = splitUpObject(12, 6);
+            // mainObject.rotation.x = toRadians(0);
+            //
+            wasSplit = false;
+            wasReflected = false;
+            dummy.add( mainObject );
+            scene.add(dummy);
+            TweenMax.from(dummy.scale, 0.5, {x:0,y:0,z:0, delay: 1});
+            // TweenMax.delayedCall(2, splitSequence);
+            animate();
 
-                mainObject.castShadow = true;
+        }
+    );
+}
 
+function splitSequence(){
+    TweenMax.to(dummy.scale, 0.5, {x:0,y:0,z:0, onComplete:function(){
+        scene.remove(dummy);
+        dummy = splitUpObject(10,5.5);
+        scene.add(dummy);
+        TweenMax.from(dummy.scale, 0.5, {x:0,y:0,z:0, delay: 0.5});
+        TweenMax.to(dummy.rotation, 20, {z:toRadians(360), repeat:-1, ease:Linear.easeNone});
+    }});
 
-                scene.add( mainObject );
-                animate();
+}
 
-            }
-        );
+function splitUpObject(numberOfClones, radius){
+    var dummy = new THREE.Mesh(new THREE.CubeGeometry(.001, .001, .001), standardMaterial);
+    var angleBetweenClones = 360/numberOfClones;
+    for(var i = 0; i<numberOfClones;i++){
+        var clone = mainObject.clone();
+        var positionAngle = angleBetweenClones*i-90
+        clone.rotation.z = toRadians(-positionAngle);
+        dummy.add(clone);
+        clone.position.x = ((i%2)+1)*(radius/2)*Math.cos(toRadians(positionAngle));
+        clone.position.y = ((i%2)+1)*(radius/2)*Math.sin(toRadians(positionAngle));
+        clone.scale.set(2/numberOfClones,2/numberOfClones,2/numberOfClones);
     }
+    return dummy;
+}
 
 function determineColour(offset){
     // var colors = [0x4E1559, 0x618897, 0x4A5825, 0x73492C, 0x77745D];
-    var colors = [0x994AC0, 0x82D6A5, 0x6017A5, 0xA791EF, 0xBC56C2, 0x4EA54D];
+    // var colors = [0x994AC0, 0x82D6A5, 0x6017A5, 0xA791EF, 0xBC56C2, 0x4EA54D];
+    var colors = [0x80FFB7, 0xf37cbe, 0xB57FFF];
     var totalTimePassed = ((performance.now() - startTime)/100) + offset;
-    var colourDuration = 30;
-    var currentColourIndex = Math.floor((totalTimePassed/colourDuration))%(colors.length-1);
+    var colourDuration = 120;
+    var currentColourIndex = Math.floor((totalTimePassed/colourDuration))%(colors.length);
     var currentColour = new THREE.Color(colors[currentColourIndex]);
-    var nextColour = new THREE.Color(colors[(currentColourIndex +1)%(colors.length-1)]);
+    var nextColour = new THREE.Color(colors[(currentColourIndex +1)%(colors.length)]);
     var alpha = (totalTimePassed%colourDuration)/colourDuration;
     return currentColour.lerp(nextColour, alpha);
+}
+
+function determineHSLColour(offset){
+    var saturation = 100;
+    var lightness = 75;
+    var colourHues = [ 146,327,265];
+    var totalTimePassed = ((performance.now() - startTime)/100) + offset;
+    var colourDuration = 120;
+    var currentColourIndex = Math.floor((totalTimePassed/colourDuration))%(colourHues.length);
+    var nextColourIndex = (currentColourIndex+1)%(colourHues.length);
+    var alpha = (totalTimePassed%colourDuration)/colourDuration;
+    var hue = (1-alpha)*colourHues[currentColourIndex] + alpha*colourHues[nextColourIndex];
+    var hslString = "hsl(" + hue + ", " + saturation + "%, " + lightness + "%)";
+    var newColour =  new THREE.Color(hslString);
+    return newColour;
 }
 
 function animate() {
@@ -124,6 +171,21 @@ function animate() {
 function update() {
     var timePassed = getTimeSinceLastFrame();
     var animationConstant = timePassed/100 ;
+    if(mainObject.rotation.y > 100 && wasSplit == false){
+        splitSequence();
+        wasSplit = true;
+    }
+    if(mainObject.rotation.y < -100 && wasReflected == false){
+        TweenMax.to(standardMaterial, 0.5, {reflectivity:0.2});
+        wasReflected = true;
+    }
+    if(mainObject.rotation.y > 0 && wasReflected == true){
+        TweenMax.to(standardMaterial, 0.5, {reflectivity:0.0});
+        wasReflected = false;
+    }
+    spotLight.color = determineHSLColour(0);
+    spotLight2.color = determineHSLColour(120);
+    spotLight3.color = determineHSLColour(240);
     renderer.render( scene, camera );
 }
 
@@ -148,16 +210,16 @@ function onMouseMove(evt) {
     if (!mouseDown) {
         return;
     }
-        var timePassed = getTimeSinceLastMoveEvent();
-        evt.preventDefault();
-        var deltaX = evt.clientX - mouseX,
-            deltaY = evt.clientY - mouseY;
-        mouseX = evt.clientX;
-        mouseY = evt.clientY;
-        if (timePassed != 0) {
-            dragMoveX = (deltaX / timePassed) / 100;
-        }
-        rotateMainObject(deltaX / 100);
+    var timePassed = getTimeSinceLastMoveEvent();
+    evt.preventDefault();
+    var deltaX = evt.clientX - mouseX,
+        deltaY = evt.clientY - mouseY;
+    mouseX = evt.clientX;
+    mouseY = evt.clientY;
+    if (timePassed != 0) {
+        dragMoveX = (deltaX / timePassed) / 100;
+    }
+    rotateMainObject(deltaX / 100);
 
 }
 
@@ -236,10 +298,20 @@ function addMouseHandler(canvas_wrapper) {
 }
 
 function rotateMainObject(amount) {
-    mainObject.rotation.y += amount;
+    // dummy.rotation.z += amount/5;
+    dummy.children.forEach(function(child){
+        child.rotation.y += amount;
+        // child.rotation.x -= amount/5;
+        // child.rotation.z += amount/5;
+    })
 }
 function rotateMainObjectByTween(){
-    mainObject.rotation.y += dragMoveX;
+    // dummy.rotation.z += dragMoveX/5;
+    dummy.children.forEach(function(child){
+        child.rotation.y += dragMoveX;
+        // child.rotation.x -= dragMoveX/5;
+        // child.rotation.z += dragMoveX/5;
+    })
 }
 
 function toRadians(deg){
